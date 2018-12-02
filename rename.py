f522d5605eda8944113d21a477c1d346fbb177b7
MichaelDavidJohnson <michaeldavid.johnson@mail.com>
2018-12-02 21:06:35 +0000

    Changed version, Now has a progress indicator to avoid an empty CMD

--- rename.py
+++ rename.py
@@ -3,12 +3,16 @@ import re
 import glob
 import shutil
 def main():
+     pathcount = 0
+     totalcount = 0
+     currentcount = 0
+     moved = None
+     namemoved = []
      pathz = []
      path1 = input("What is the first file path you want to use:\n")
      pathz.append(path1)
      path2 = input("What is the second file path you want to use:\n")
      pathz.append(path2)
-     
      text = input("Do you want to remove square brackets? Y/N\n")
      if text == 'Y' or text == 'y':
           for path in pathz:
@@ -17,19 +21,15 @@ def main():
                pattern2 = r' \[[^\]]*\]'
                pattern3 = r'\[[^\]]*\]'
                for name in files:
-                    #print(name)
+                    totalcount += 1
                     new_name = re.sub(pattern, r'', name)
                     new_name2 = re.sub(pattern2,r'',new_name)
                     new_name3 = re.sub(pattern3,r'',new_name2)
-                    
-                    #print(new_name)
                     os.rename(os.path.join(path,name), os.path.join(path,new_name))
                     os.rename(os.path.join(path,new_name),os.path.join(path,new_name2))
                     os.rename(os.path.join(path,new_name2),os.path.join(path,new_name3))
      elif text == 'N' or text =='n':
           pass
-     #for i in range(26):
-          #if i < 14
      condition2 = input("Do you want to move the files? Y/N\n")
      if condition2 == 'Y' or condition2 == 'y':
           if len(pathz) == 1:
@@ -37,21 +37,31 @@ def main():
 
           for path in pathz:
                files = os.listdir(path)
+
                for name in files:     
-               
+
+                    if not name in namemoved:
+                         currentcount+=1
+                         print("Progress :", currentcount, "/", totalcount)
+                         print("Files are moving, do not close the window")
+
                     for i in range(25,-1,-1):
                          letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
                          Cletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-               
-                    
+
+                  
+
                          if name[0] == letters[i] or name[0] == Cletters[i]:
-                         
+    
                                    if i > 13:
                                         targetpath = pathz[1]
                                         if os.path.join(targetpath,name) == os.path.join(path, name):
+
                                              continue
                                         else:
                                              shutil.move(os.path.join(path, name),targetpath)
+                                             namemoved.append(name)
+                                            
                               
                                    else:
                                         targetpath = pathz[0]
@@ -59,9 +69,16 @@ def main():
                                              continue
                                         else:
                                              shutil.move(os.path.join(path, name),targetpath)
+                                             namemoved.append(name)
+
+
+                    if len(namemoved) == 0:
+                         print("Nothing needs to be moved!")
+                         
+
      if condition2 == 'n' or condition2 == 'N':
           pass
                    
 if __name__ == '__main__':
      main()
-     
+     input("Press ENTER to exit")     
